58ebfd67595abc2c56e7b57384b914115096ab1e
JFMajer <jakubmajer@live.com>
2023-01-27 12:43:38 +0100

    get all books lambda function

---
+++ get-all-books/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+type Book struct {
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	ISBN        string `json:"isbn"`
+	Description string `json:"description"`
+}
+
+func getAllBooks() (events.APIGatewayProxyResponse, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-north-1"))
+	if err != nil {
+		log.Fatalf("failed to load configuration, %v", err)
+	}
+
+	client := dynamodb.NewFromConfig(cfg)
+	req, err := client.Scan(context.TODO(), &dynamodb.ScanInput{
+		TableName: aws.String(os.Getenv("TABLE_NAME")),
+	})
+	if err != nil {
+		log.Fatalf("failed to scan table, %v", err)
+	}
+
+	books := make([]Book, 0)
+	err = attributevalue.UnmarshalListOfMaps(req.Items, &books)
+	if err != nil {
+		log.Fatalf("failed to unmarshal books, %v", err)
+	}
+
+	body, err := json.Marshal(books)
+	if err != nil {
+		log.Fatalf("failed to marshal books, %v", err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(body),
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(getAllBooks)
+}
